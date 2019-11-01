b9b540e18ff3b969253fcf958ef078cf9bff2ea4
seomkim <thseontw@gmail.com>
2019-11-01 21:34:37 +0900

    apply react-router

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
   {
@@ -33,51 +34,56 @@ const categories = [
 ];
 
 const CategoriesBlock = styled.div`
-    display: flex;
-    padding: 1rem;
-    width: 768px;
-    margin: 0 auto;
-    @media screen and (max-width: 768px) {
-        width: 100%;
-        overflow-x: auto;
-    }
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
 `;
 
-const Category = styled.div`
-    font-size: 1.125rem;
-    cursor: pointer;
-    white-space: pre;
-    text-decoration: none;
-    color: inherit;
-    padding-bottom: 0.25rem;
+const Category = styled(NavLink)`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
 
+  &:hover {
+    color: #495057;
+  }
+
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
     &:hover {
-        color: #495057;
+      color: #3bc9db;
     }
+  }
 
-    ${props =>
-        props.active && css`
-            font-weight: 600;
-            border-bottom: 2px solid #22b8cf;
-            color: #22b8cf;
-            &:hover {
-                color: #3bc9db;
-            }
-    `}
-
-    & + & {
-        margin-left: 1rem;
-    }
+  & + & {
+    margin-left: 1rem;
+  }
 `;
-
 
 const Categories = ({ category, onSelect }) => {
   return (
-      <CategoriesBlock>
-        {categories.map(c => (
-            <Category key={c.name} active={category === c.name} onClick={() => onSelect(c.name)}>{c.text}</Category>
-        ))}
-      </CategoriesBlock>
+    <CategoriesBlock>
+      {categories.map(c => (
+        <Category
+          key={c.name}
+          activeClassName="active"
+          exact={c.name === 'all'}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
+        >
+          {c.text}
+        </Category>
+      ))}
+    </CategoriesBlock>
   );
 };
 
